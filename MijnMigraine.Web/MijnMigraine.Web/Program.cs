f96bfeb3f20c76e88ec0bd7cfca0561d6695bdb1
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2024-08-26 16:00:28 +0200

    Added CI pipeline

--- MijnMigraine.Web/MijnMigraine.Web/Program.cs
+++ MijnMigraine.Web/MijnMigraine.Web/Program.cs
@@ -10,6 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddEnvironmentVariables();
 
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    var certificateFilename = context.Configuration.GetValue<string>("CERTIFICATE_FILENAME");
+    var certificatePassword = context.Configuration.GetValue<string>("CERTIFICATE_PASSWORD");
+    var port = context.Configuration.GetValue<int?>("PORT") ?? 8080;
+
+    if (certificateFilename == null)
+    {
+        options.Listen(IPAddress.Any, port);
+    }
+    else
+    {
+        options.Listen(IPAddress.Any, port, listenOption => listenOption.UseHttps(certificateFilename, certificatePassword));
+    }
+});
+
 builder.Services.AddMudServices();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
